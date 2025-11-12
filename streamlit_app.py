84f841bcfd9ebe14dec8a33cac69396a66d9e3d8
Nuggies <160486085+nugg1ess@users.noreply.github.com>
2025-11-12 11:50:15 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+#st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+
+st.title("Toolmess + Corrosive Essence")
